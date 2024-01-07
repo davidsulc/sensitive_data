89bde4afd2c87b9149fb505c22905461ffff9321
David Sulc <davidsulc@gmail.com>
2024-01-07 15:10:32 +0100

    typo

--- lib/sensitive_data/redacted.ex
+++ lib/sensitive_data/redacted.ex
@@ -1,5 +1,5 @@
 defmodule SensitiveData.Redacted do
   @moduledoc """
-  An empty module seriving only to indicate data that has been redacted.
+  An empty module serving only to indicate data that has been redacted.
   """
 end
