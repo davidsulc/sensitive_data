0f53898a5b503e19bfd204b0525f995139313638
David Sulc <davidsulc@gmail.com>
2024-01-12 10:58:33 +0100

    add test

--- lib/sensitive_data/redaction.ex
+++ lib/sensitive_data/redaction.ex
@@ -9,6 +9,22 @@ defmodule SensitiveData.Redaction do
 
   alias SensitiveData.Redacted
 
+  @type pruning_strategy :: redactor_fun() | pruning_strategy_name()
+  @typedoc """
+  A function responsible for redacting term and args.
+  It will be given a value to redact, as well whether the value came
+  from the `:term` or `:args` key within the `Exception` struct.
+  The function must return a redacted version of the provided value.
+  """
+  @type redactor_fun :: (term(), value_type() -> term())
+  @typedoc """
+  The name of the pruning strategy to use when pruning `term` and `args`
+  values from `Exception`s. Currently, only `:strip` is supported.
+  """
+  @type pruning_strategy_name :: :strip
+  @typedoc "The type of information being pruned from the `Exception`"
+  @type value_type :: :term | :args
+
   @doc """
   Prunes term and arguments from the given exception.
 
@@ -23,16 +39,45 @@ defmodule SensitiveData.Redaction do
       %BadMapError{term: "SOME SECRET"}
       iex> SensitiveData.Redaction.prune_exception(exception)
       %BadMapError{term: SensitiveData.Redacted}
+      iex> SensitiveData.Redaction.prune_exception(exception, fn val, type ->
+      ...>   case type do
+      ...>     :term ->
+      ...>       {h, t} = String.split_at(val, 3)
+      ...>       IO.iodata_to_binary([h, List.duplicate("*", String.length(t))])
+      ...>     :args -> "*** redacted args ***"
+      ...>   end
+      ...> end)
+      %BadMapError{term: "SOM********"}
   """
-  @spec prune_exception(map()) :: map()
-  def prune_exception(e) when is_exception(e),
-    do: e |> prune_term() |> prune_args()
+  @spec prune_exception(Exception.t(), pruning_strategy()) :: Exception.t()
+  def prune_exception(e, pruning_strategy \\ :strip) when is_exception(e) do
+    redactor_fun =
+      case pruning_strategy do
+        :strip -> &strip/2
+        fun when is_function(fun, 2) -> fun
+      end
 
-  defp prune_term(%{term: _} = e), do: %{e | term: Redacted}
-  defp prune_term(e), do: e
+    e
+    |> prune_term(redactor_fun)
+    |> prune_args(redactor_fun)
+  end
 
-  defp prune_args(%{args: args} = e), do: %{e | args: List.duplicate(Redacted, length(args))}
-  defp prune_args(e), do: e
+  @spec prune_term(Exception.t(), redactor_fun()) :: Exception.t()
+
+  defp prune_term(%{term: term} = e, redactor_fun) when is_function(redactor_fun, 2),
+    do: %{e | term: redactor_fun.(term, :term)}
+
+  defp prune_term(e, _redactor_fun), do: e
+
+  @spec prune_args(Exception.t(), redactor_fun()) :: Exception.t()
+
+  defp prune_args(%{args: args} = e, redactor_fun) when is_function(redactor_fun, 2),
+    do: %{e | args: redactor_fun.(args, :args)}
+
+  defp prune_args(e, _redactor_fun), do: e
+
+  defp strip(_term, :term), do: Redacted
+  defp strip(args, :args), do: List.duplicate(Redacted, length(args))
 
   ############ Code taken from plug_crypto ########################################
   #
--- test/sensitive_data/redaction_test.exs
+++ test/sensitive_data/redaction_test.exs
@@ -1,4 +1,17 @@
 defmodule SensitiveData.RedactionTest do
   use ExUnit.Case
   doctest SensitiveData.Redaction
+
+  import SensitiveData.Redaction
+
+  test "prune_exception" do
+    pruned_exception =
+      try do
+        Map.get("SOME SECRET", :some_key)
+      rescue
+        e -> prune_exception(e)
+      end
+
+    assert %BadMapError{term: SensitiveData.Redacted} = pruned_exception
+  end
 end
