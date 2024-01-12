4faca72b5222a932bdf679394359dbe5cd5b9cf7
David Sulc <davidsulc@gmail.com>
2024-01-12 10:55:23 +0100

    add docs

--- lib/sensitive_data/redaction.ex
+++ lib/sensitive_data/redaction.ex
@@ -1,6 +1,29 @@
 defmodule SensitiveData.Redaction do
+  @moduledoc """
+  Functions for redacting information.
+
+  In typical use, there's no need to call these functions directly:
+  `SensitiveData.execute/1` should be used instead as it will handle
+  redaction duties and call the necessary functions from this module.
+  """
+
   alias SensitiveData.Redacted
 
+  @doc """
+  Prunes term and arguments from the given exception.
+
+  ## Example
+
+      iex> exception =
+      ...>   try do
+      ...>     Map.get("SOME SECRET", :some_key)
+      ...>   rescue
+      ...>     e -> e
+      ...>   end
+      %BadMapError{term: "SOME SECRET"}
+      iex> SensitiveData.Redaction.prune_exception(exception)
+      %BadMapError{term: SensitiveData.Redacted}
+  """
   @spec prune_exception(map()) :: map()
   def prune_exception(e) when is_exception(e),
     do: e |> prune_term() |> prune_args()
