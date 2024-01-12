1adeb07fea7142343f4fb4fc5e76c152a922c8b9
David Sulc <davidsulc@gmail.com>
2024-01-12 12:59:01 +0100

    refactor tests

--- test/sensitive_data/redaction_test.exs
+++ test/sensitive_data/redaction_test.exs
@@ -4,29 +4,46 @@ defmodule SensitiveData.RedactionTest do
 
   import SensitiveData.Redaction
 
-  test "prune_exception" do
-    pruned_exception =
+  setup do
+    secret = "SOME SECRET"
+
+    {exception, stacktrace} =
       try do
-        Map.get("SOME SECRET", :some_key)
+        Map.get(secret, :some_key)
       rescue
-        e -> prune_exception(e)
+        e -> {e, __STACKTRACE__}
       end
 
-    assert %BadMapError{term: SensitiveData.Redacted} = pruned_exception
+    %{
+      secret: secret,
+      exception: exception,
+      stacktrace: stacktrace
+    }
   end
 
-  test "prune_args_from_stacktrace" do
+  test "prune_exception/2", %{secret: secret, exception: exception} do
+    assert %BadMapError{term: ^secret} = exception
+    assert %BadMapError{term: SensitiveData.Redacted} = prune_exception(exception)
+
+    redactor = fn val, type ->
+      case type do
+        :term ->
+          {h, t} = String.split_at(val, 3)
+          IO.iodata_to_binary([h, List.duplicate("*", String.length(t))])
+
+        :args ->
+          "*** redacted args ***"
+      end
+    end
+
+    assert %BadMapError{term: "SOM********"} = prune_exception(exception, redactor)
+  end
+
+  test "prune_args_from_stacktrace/2", %{secret: secret, stacktrace: stacktrace} do
     # we only care about the last call (and drop the file info from the tuple)
     show_last = fn stacktrace -> stacktrace |> hd() |> Tuple.delete_at(3) end
 
-    stacktrace =
-      try do
-        Map.get("SOME SECRET", :some_key)
-      rescue
-        _ -> __STACKTRACE__
-      end
-
-    assert {Map, :get, ["SOME SECRET", :some_key, nil]} = show_last.(stacktrace)
+    assert {Map, :get, [^secret, :some_key, nil]} = show_last.(stacktrace)
 
     assert {Map, :get, 3} =
              stacktrace
