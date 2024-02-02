32809e3256e9094b85bba540e34ea406f9df6b07
David Sulc <davidsulc@gmail.com>
2024-02-02 17:22:12 +0100

    reduce Elixir version to 1.11

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SensitiveData.MixProject do
     [
       app: :sensitive_data,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [
